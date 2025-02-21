109357eb715dcb33e8f4bc9e31303130153a309d
LiuQi <1139642444@qq.com>
2025-02-21 10:39:27 +0800

    SharpBladeUtilsCore 模块 ArrayUtil 数组处理工具类型 新增 新增 isEmpty 数组是否为空方法 -

---
+++ SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/array/ArrayUtil.java
@@ -0,0 +1,24 @@
+package org.chino.SharpBladeUtils.core.array;
+
+/**
+ * @ClassName ArrayUtil
+ * @Description ArrayUtil 数组处理工具
+ * @Author LiuQi
+ * @Date 2025/2/21 10:30
+ * @Version 1.0
+ */
+public class ArrayUtil {
+
+    /**
+     * isEmpty 数组是否为空
+     *
+     * @param array {@link T[]} 数组
+     * @param <T>   泛型类型
+     * @return 返回 数组为空 或者 数组长度为0 的布尔值  true：数组为空 或者 数组长度为0  false：反之
+     * @author LiuQi
+     */
+    public static <T> boolean isEmpty(final T[] array) {
+        // 返回 数组为空 或者 数组长度为0 的布尔值
+        return array == null || array.length == 0;
+    }
+}
