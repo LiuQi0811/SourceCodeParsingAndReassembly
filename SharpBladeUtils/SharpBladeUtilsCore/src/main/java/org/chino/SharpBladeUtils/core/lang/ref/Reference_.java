d889b2907f65c78846ebe3e2bbcea3ee352324bf
LiuQi <1139642444@qq.com>
2025-02-22 17:03:40 +0800

    SharpBladeUtilsCore 模块 新增 Reference_ 针对{@link java.lang.ref.Reference}的接口定义，用于扩展功能接口 新增 get 获取引用的原始对象接口方法 -

---
+++ SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/lang/ref/Reference_.java
@@ -0,0 +1,23 @@
+package org.chino.SharpBladeUtils.core.lang.ref;
+
+/**
+ * @ClassName Reference_
+ * @Description Reference_ 针对{@link java.lang.ref.Reference}的接口定义，用于扩展功能
+ * <pre>
+ *     1. 扩展Reference的功能
+ *     2. 提供自定义的无需回收对象
+ * </pre>
+ * @Author LiuQi
+ * @Date 2025/2/21 13:12
+ * @Version 1.0
+ */
+@FunctionalInterface
+public interface Reference_<T> {
+    /**
+     * get 获取引用的原始对象
+     *
+     * @return T {@link T} 原始对象
+     * @author LiuQi
+     */
+    T get();
+}
