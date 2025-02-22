15cb417cddc388e3481556a35feb4ba6cef6b31b
LiuQi <1139642444@qq.com>
2025-02-22 17:05:30 +0800

    SharpBladeUtilsCore 模块 新增SafeConcurrentHashMap 线程安全的ConcurrentHashMap实现类 新增  SafeConcurrentHashMap 构造方法 -

---
+++ SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/map/concurrent/SafeConcurrentHashMap.java
@@ -0,0 +1,28 @@
+package org.chino.SharpBladeUtils.core.map.concurrent;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+/**
+ * @ClassName SafeConcurrentHashMap
+ * @Description SafeConcurrentHashMap 线程安全的ConcurrentHashMap实现
+ * <pre>
+ *     用于解决在JDK8中调用{@link ConcurrentHashMap#computeIfAbsent(Object, Function)}可能造成的死循环问题。<br>
+ * </pre>
+ * @Author LiuQi
+ * @Date 2025/2/21 13:04
+ * @Version 1.0
+ */
+public class SafeConcurrentHashMap<K, V> extends ConcurrentHashMap<K, V> {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * SafeConcurrentHashMap 构造方法
+     *
+     * @author LiuQi
+     */
+    public SafeConcurrentHashMap() {
+        // 调用{@code ConcurrentHashMap}构造方法
+        super();
+    }
+}
