9640bc9d743ce7d0ec8bf2539c8eec685cfba6ff
LiuQi <1139642444@qq.com>
2025-02-22 10:01:43 +0800

    SharpBladeUtilsCore 模块 WeakConcurrentMap线程安全的WeakMap实现类 新增 新增 WeakConcurrentMap 构造方法 - 新增 ReferenceConcurrentMap 抽象类 实现方法 逻辑待优化 -

---
+++ SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/map/reference/WeakConcurrentMap.java
@@ -0,0 +1,53 @@
+package org.chino.SharpBladeUtils.core.map.reference;
+
+import org.chino.SharpBladeUtils.core.lang.ref.Reference_;
+import org.chino.SharpBladeUtils.core.map.concurrent.SafeConcurrentHashMap;
+
+import java.lang.ref.ReferenceQueue;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * @ClassName WeakConcurrentMap
+ * @Description WeakConcurrentMap 线程安全的WeakMap实现
+ * 键和值都为Weak引用，即，在GC时发现弱引用会回收其对象
+ * @Author LiuQi
+ * @Date 2025/2/21 11:47
+ * @Version 1.0
+ */
+public class WeakConcurrentMap<K, V> extends ReferenceConcurrentMap<K, V> {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * WeakConcurrentMap 构造方法
+     *
+     * @author LiuQi
+     */
+    public WeakConcurrentMap() {
+        // 默认使用SafeConcurrentHashMap
+        this(new SafeConcurrentHashMap<>());
+    }
+
+    /**
+     * WeakConcurrentMap 构造方法
+     *
+     * @param dataMap ConcurrentMap
+     * @author LiuQi
+     */
+    public WeakConcurrentMap(final ConcurrentMap<Reference_<K>, Reference_<V>> dataMap) {
+        // 调用 {@code ReferenceConcurrentMap}构造方法
+        super(dataMap);
+    }
+
+    @Override
+    Reference_<K> wrapKey(K key, ReferenceQueue<? super K> referenceQueue) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    Reference_<V> wrapValue(V value, ReferenceQueue<? super V> referenceQueue) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
