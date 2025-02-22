ffa451a5176ea4fcfc7fb4065111df9357bdd867
LiuQi <1139642444@qq.com>
2025-02-22 11:58:36 +0800

    SharpBladeUtilsCore 模块 ObjectUtil对象处理工具 新增 新增defaultIfNull 是否为空则返回默认值方法 -

--- SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/util/ObjectUtil.java
+++ SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/util/ObjectUtil.java
@@ -1,5 +1,7 @@
 package org.chino.SharpBladeUtils.core.util;
 
+import java.util.function.Function;
+
 /**
  * @ClassName ObjectUtil
  * @Description ObjectUtil 对象处理工具
@@ -21,8 +23,25 @@ public class ObjectUtil {
      * @return {@link Boolean} 对象是否为空 true:空 false:非空
      * @author LiuQi
      */
-    public static boolean isNull(Object value) {
+    public static boolean isNull(final Object value) {
         // 返回对象是否为空 true:空 false:非空
         return null == value;
+    }
+
+    /**
+     * defaultIfNull 是否为空则返回默认值
+     *
+     * @param value         {@link Object} 对象
+     * @param customHandler {@link Function} 自定义处理器
+     * @param defaultValue  {@link R} 默认值
+     * @param <T>           {@link T}     默认值类型
+     * @param <R>           {@link R}    返回值类型
+     * @return {@link R} 返回值
+     * @description 返回自定义handler处理后的结果，否则返回默认值
+     * @author LiuQi
+     */
+    public static <T, R> R defaultIfNull(final T value, final Function<? super T, ? extends R> customHandler, final R defaultValue) {
+        // 返回参数对象为空返回默认值，否则返回自定义handler处理后的结果。
+        return isNull(value) ? defaultValue : customHandler.apply(value);
     }
 }
