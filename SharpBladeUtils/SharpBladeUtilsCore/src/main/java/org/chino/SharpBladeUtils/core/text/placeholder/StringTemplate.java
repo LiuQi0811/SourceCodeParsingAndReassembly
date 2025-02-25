14c72e5031b7228f08b87b580df7dd696b32e84d
LiuQi <1139642444@qq.com>
2025-02-25 10:40:19 +0800

    SharpBladeUtilsCore 模块
    StringTemplate 字符串模板类
    新增 globalFeatures 全局策略值 -
    
    AbstractBuilder 抽象构建器
    新增template 字符串模板,features 策略值属性
    AbstractBuilder 构造方法 设置字符串模板和策略值初始化 -
    
    Feature 策略枚举类新增
    新增 Feature 构造方法 位运算计算二进掩码值 -
    新增 set 设置策略值方法 -
    新增 of 组合策略值方法 -

--- SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/text/placeholder/StringTemplate.java
+++ SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/text/placeholder/StringTemplate.java
@@ -2,6 +2,10 @@ package org.chino.SharpBladeUtils.core.text.placeholder;
 
 import org.chino.SharpBladeUtils.core.text.placeholder.template.SinglePlaceholderStringTemplate;
 
+import java.util.Objects;
+
+import static org.chino.SharpBladeUtils.core.text.placeholder.StringTemplate.Feature.*;
+
 /**
  * @ClassName StringTemplate
  * @Description StringTemplate 字符串模板
@@ -10,6 +14,12 @@ import org.chino.SharpBladeUtils.core.text.placeholder.template.SinglePlaceholde
  * @Version 1.0
  */
 public abstract class StringTemplate {
+
+    /**
+     * globalFeatures 全局策略值
+     */
+    protected static int globalFeatures = Feature.of(FORMAT_MISSING_KEY_PRINT_WHOLE_PLACEHOLDER, FORMAT_NULL_VALUE_TO_STR,
+            MATCH_KEEP_DEFAULT_VALUE, MATCH_EMPTY_VALUE_TO_NULL, MATCH_NULL_STR_TO_NULL);
 
     /**
      * of 构建模板
@@ -35,6 +45,14 @@ public abstract class StringTemplate {
      * @Author LiuQi
      */
     protected static abstract class AbstractBuilder<BuilderChild extends AbstractBuilder<BuilderChild, TemplateChild>, TemplateChild extends StringTemplate> {
+        /**
+         * template 字符串模板
+         */
+        protected final String template;
+        /**
+         * features 策略值
+         */
+        protected int features;
 
         /**
          * AbstractBuilder 构造方法
@@ -43,7 +61,98 @@ public abstract class StringTemplate {
          * @author LiuQi
          */
         protected AbstractBuilder(final String template) {
-            System.out.println("AbstractBuilder 执行啦 " + template);
+            // 校验模板不为空 设置字符串模板和策略值
+            this.template = Objects.requireNonNull(template);
+            this.features = StringTemplate.globalFeatures;
+        }
+    }
+
+    /**
+     * @ClassName Feature
+     * @Description Feature 策略枚举
+     * @Author LiuQi
+     */
+    public enum Feature {
+        /**
+         * FORMAT_MISSING_KEY_PRINT_WHOLE_PLACEHOLDER 缺失键时打印整个占位符
+         */
+        FORMAT_MISSING_KEY_PRINT_WHOLE_PLACEHOLDER(0, 0, 6),
+        /**
+         * FORMAT_MISSING_KEY_PRINT_EMPTY 缺失键时打印空字符串
+         */
+        FORMAT_MISSING_KEY_PRINT_EMPTY(3, 0, 6),
+        /**
+         * FORMAT_NULL_VALUE_TO_STR 空值转为字符串 "null"
+         */
+        FORMAT_NULL_VALUE_TO_STR(6, 6, 4),
+        /**
+         * MATCH_KEEP_DEFAULT_VALUE 匹配时保持默认值，不替换为空字符串
+         */
+        MATCH_KEEP_DEFAULT_VALUE(16, 16, 3),
+        /**
+         * MATCH_EMPTY_VALUE_TO_NULL 匹配时空值转为 null
+         */
+        MATCH_EMPTY_VALUE_TO_NULL(19, 19, 4),
+        /**
+         * MATCH_NULL_STR_TO_NULL 匹配时字符串 "null" 转为 null
+         */
+        MATCH_NULL_STR_TO_NULL(23, 23, 3),
+        ;
+
+        /**
+         * mask 掩码值
+         */
+        private final int mask;
+        /**
+         * clearMask 清除掩码值
+         */
+        private final int clearMask;
+
+        /**
+         * Feature 构造方法
+         *
+         * @param binaryPosition 二进制位置
+         * @param binaryStart    二进制起始位
+         * @param binaryLength   二进制长度
+         * @author LiuQi
+         */
+        Feature(final int binaryPosition, final int binaryStart, final int binaryLength) {
+            // 1 << binaryPosition;  例如：1 << 0 = 1, 1 << 1 = 2
+            this.mask = 1 << binaryPosition;
+            // (-1 << (binaryStart + binaryLength)) | ((1 << binaryStart) - 1);  例如：(-1 << (2 + 3)) | ((1 << 2) - 1);
+            this.clearMask = (-1 << (binaryStart + binaryLength)) | ((1 << binaryStart) - 1);
+        }
+
+        /**
+         * set 设置策略值
+         *
+         * @param features 策略值
+         * @return 返回策略值
+         * @author LiuQi
+         */
+        public int set(final int features) {
+            // 返回策略值，将当前枚举值的掩码值设置为 features 的掩码值 & clearMask & mask
+            return (features & clearMask) | mask;
+        }
+
+        /**
+         * of 组合策略值
+         *
+         * @param features 策略值数组
+         * @return 返回组合后的策略值
+         * @author LiuQi
+         */
+        public static int of(final Feature... features) {
+            // 如果 features 为空，则返回 0
+            if (null == features) return 0;
+            // 定义 result，用于存储组合后的策略值
+            int result = 0;
+            for (Feature feature : features) { // 遍历 features 数组，将每个枚举值对应的掩码值设置为 result
+                // 将枚举值对应的掩码值设置为 result
+                result = feature.set(result);
+            }
+            // 返回组合后的策略值
+            return result;
         }
     }
 }
