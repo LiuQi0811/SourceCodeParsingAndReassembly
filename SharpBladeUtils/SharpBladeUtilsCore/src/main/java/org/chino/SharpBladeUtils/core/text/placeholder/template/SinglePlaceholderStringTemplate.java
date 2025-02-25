82e4cb484d453d6ba822777214fa2a0b7260150e
LiuQi <1139642444@qq.com>
2025-02-25 15:47:32 +0800

    SharpBladeUtilsCore 模块 SinglePlaceholderStringTemplate 单个占位符字符串模板类 新增 DEFAULT_PLACEHOLDER 默认占位符 placeholder 占位符{@link  StringPool#EMPTY_JSON} 新增SinglePlaceholderStringTemplate 构造方法 待优化 - 新增 buildInstance抽象方法实现 -

--- SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/text/placeholder/template/SinglePlaceholderStringTemplate.java
+++ SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/text/placeholder/template/SinglePlaceholderStringTemplate.java
@@ -1,6 +1,9 @@
 package org.chino.SharpBladeUtils.core.text.placeholder.template;
 
+import org.chino.SharpBladeUtils.core.text.StringPool;
 import org.chino.SharpBladeUtils.core.text.placeholder.StringTemplate;
+
+import java.util.function.UnaryOperator;
 
 /**
  * @ClassName SinglePlaceholderStringTemplate
@@ -13,7 +16,33 @@ import org.chino.SharpBladeUtils.core.text.placeholder.StringTemplate;
  * @Version 1.0
  */
 public class SinglePlaceholderStringTemplate extends StringTemplate {
+    /**
+     * DEFAULT_PLACEHOLDER 默认占位符
+     */
+    public static final String DEFAULT_PLACEHOLDER = StringPool.EMPTY_JSON;
 
+    /**
+     * placeholder 占位符{@link  StringPool#EMPTY_JSON}
+     */
+    protected String placeholder;
+
+    /**
+     * SinglePlaceholderStringTemplate 构造方法
+     * @param placeholder  {@link String} 占位符
+     * @param template   {@link String} 字符串模板
+     * @param escapeChar 转义字符
+     * @param defaultValue {@link String} 默认值
+     * @param defaultValueHandler {@link UnaryOperator<String>} 默认值处理器
+     * @param features    策略值
+     * @author LiuQi
+     */
+    protected SinglePlaceholderStringTemplate(final String placeholder, final String template, final char escapeChar, final String defaultValue, final UnaryOperator<String> defaultValueHandler, final int features) {
+        // 调用{@code StringTemplate(template, escapeChar, defaultValue, defaultValueHandler, features)} 构造方法
+        super(template, escapeChar, defaultValue, defaultValueHandler, features);
+        // 设置占位符
+        this.placeholder = placeholder;
+        // TODO  Segment列表 initialize
+    }
 
     /**
      * builder 构建器
@@ -49,6 +78,14 @@ public class SinglePlaceholderStringTemplate extends StringTemplate {
         public Builder(final String template) {
             // 调用 {@code AbstractBuilder(template)}构造方法
             super(template);
+        }
+
+        @Override
+        protected SinglePlaceholderStringTemplate buildInstance() {
+            // 如果占位符为空，则使用默认占位符
+            if (placeholder == null) this.placeholder = DEFAULT_PLACEHOLDER;
+            // 返回 SinglePlaceholderStringTemplate 对象
+            return new SinglePlaceholderStringTemplate(placeholder, template, escapeChar, defaultValue, defaultValueHandler, features);
         }
 
         /**
