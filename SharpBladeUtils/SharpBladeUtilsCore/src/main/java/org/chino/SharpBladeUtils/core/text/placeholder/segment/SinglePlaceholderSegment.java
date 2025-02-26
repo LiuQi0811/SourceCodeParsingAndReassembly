ba12e23576260aa843fc23f647de604967568da7
LiuQi <1139642444@qq.com>
2025-02-26 17:45:58 +0800

    SharpBladeUtilsCore 模块 SinglePlaceholderSegment 字符串模板-单变量占位符 Segment类 新增 of 创建SinglePlaceholderSegment 对象方法 -

--- SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/text/placeholder/segment/SinglePlaceholderSegment.java
+++ SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/text/placeholder/segment/SinglePlaceholderSegment.java
@@ -16,6 +16,19 @@ public class SinglePlaceholderSegment extends AbstractPlaceholderSegment {
      * @author LiuQi
      */
     public SinglePlaceholderSegment(final String placeholder) {
+        // 调用 {@code AbstractPlaceholderSegment#AbstractPlaceholderSegment(String)} 构造方法
         super(placeholder);
+    }
+
+    /**
+     * of  创建SinglePlaceholderSegment 对象
+     *
+     * @param placeholder {@link String} 占位符字符串
+     * @return {@link SinglePlaceholderSegment} SinglePlaceholderSegment 字符串模板-单变量占位符 Segment对象
+     * @author LiuQi
+     */
+    public static SinglePlaceholderSegment of(final String placeholder) {
+        // 创建SinglePlaceholderSegment对象并返回
+        return new SinglePlaceholderSegment(placeholder);
     }
 }
