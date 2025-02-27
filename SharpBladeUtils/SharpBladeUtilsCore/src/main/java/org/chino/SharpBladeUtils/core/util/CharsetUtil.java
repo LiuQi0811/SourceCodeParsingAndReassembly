39bdf8c7e1e5fae8ca8b478df7e5fa7ca16630c7
LiuQi <1139642444@qq.com>
2025-02-27 13:35:11 +0800

    SharpBladeUtilsCore 模块 CharsetUtil Charset字符集处理工具类 新增 新增 UTF-8 字符集 属性 -

---
+++ SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/util/CharsetUtil.java
@@ -0,0 +1,18 @@
+package org.chino.SharpBladeUtils.core.util;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @ClassName CharsetUtil
+ * @Description CharsetUtil Charset字符集处理工具
+ * @Author LiuQi
+ * @Date 2025/2/27 13:06
+ * @Version 1.0
+ */
+public class CharsetUtil {
+    /**
+     * UTF-8 字符集
+     */
+    public static final Charset UTF_8 = StandardCharsets.UTF_8;
+}
