1f63b8fb62b4fa196ed1fa46c9adc30d5293fb81
LiuQi <1139642444@qq.com>
2025-07-25 11:28:39 +0800

    PrimitiveArrayUtil.java ADD => isNotEmpty 判断一个 long 类型的数组是否非空 方法 ADD =>  isNotEmpty 判断一个 short 类型的数组是否非空 方法 ADD =>   isNotEmpty 判断一个 char 类型的数组是否非空 方法 ADD =>  isNotEmpty 判断一个 byte 类型的数组是否非空 方法 ADD => isNotEmpty 判断一个 double 类型的数组是否非空 方法 ADD =>  isNotEmpty 判断一个 float 类型的数组是否非空 方法 ADD =>   isNotEmpty 判断一个 boolean 类型的数组是否非空 方法 ADD =>  isEmpty 检查给定的long类型数组是否为空（null或长度为0） 方法 ADD => isEmpty 检查给定的 double 类型数组是否为空（null 或长度为 0） 方法 ADD =>  isEmpty 检查给定的 float 类型数组是否为空（null 或长度为 0） 方法 ADD =>  isEmpty 检查给定的 boolean 类型数组是否为空（null 或长度为 0）  方法 ADD =>  isEmpty 检查给定的 short 类型数组是否为空（null 或长度为 0）方法 ADD =>  isEmpty 检查给定的 char 类型数组是否为空（null 或长度为 0） 方法 ADD =>  isEmpty 检查给定的 byte 类型数组是否为空（null 或长度为 0） 方法 ADD =>  max 计算一个可变参数 int 类型数组中的最大值  方法 ADD =>   max 计算一个可变参数 long 类型数组中的最大值 方法 ADD => max 计算一个可变参数 double 类型数组中的最大值 方法

--- SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/util/PrimitiveArrayUtil.java
+++ SharpBladeUtils/SharpBladeUtilsCore/src/main/java/org/chino/SharpBladeUtils/core/util/PrimitiveArrayUtil.java
@@ -1,6 +1,7 @@
 package org.chino.SharpBladeUtils.core.util;
 
 
+
 /**
  * @ClassName PrimitiveArrayUtil
  * @Description PrimitiveArrayUtil 原始数组工具
@@ -32,6 +33,104 @@ public class PrimitiveArrayUtil {
     }
 
     /**
+     * isNotEmpty 判断一个 long 类型的数组是否非空。
+     * 如果数组不为 null 且长度大于 0，则返回 true，表示数组非空；
+     * 否则返回 false，表示数组为空。
+     *
+     * @param array {@link Long[]}要检查的 long 类型数组
+     * @return 如果数组非空则返回 true，否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isNotEmpty(long[] array) {
+        // 调用 isEmpty 方法并取反，判断数组是否非空
+        return !isEmpty(array);
+    }
+
+    /**
+     * isNotEmpty 判断一个 short 类型的数组是否非空。
+     * 如果数组不为 null 且长度大于 0，则返回 true，表示数组非空；
+     * 否则返回 false，表示数组为空。
+     *
+     * @param array {@link Short[]}要检查的 short 类型数组
+     * @return 如果数组非空则返回 true，否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isNotEmpty(short[] array) {
+        // 调用 isEmpty 方法并取反，判断数组是否非空
+        return !isEmpty(array);
+    }
+
+    /**
+     * isNotEmpty 判断一个 char 类型的数组是否非空。
+     * 如果数组不为 null 且长度大于 0，则返回 true，表示数组非空；
+     * 否则返回 false，表示数组为空。
+     *
+     * @param array {@link Character[]}要检查的 char 类型数组
+     * @return 如果数组非空则返回 true，否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isNotEmpty(char[] array) {
+        // 调用 isEmpty 方法并取反，判断数组是否非空
+        return !isEmpty(array);
+    }
+
+    /**
+     * isNotEmpty 判断一个 byte 类型的数组是否非空。
+     * 如果数组不为 null 且长度大于 0，则返回 true，表示数组非空；
+     * 否则返回 false，表示数组为空。
+     *
+     * @param array {@link Byte[]}要检查的 byte 类型数组
+     * @return 如果数组非空则返回 true，否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isNotEmpty(byte[] array) {
+        // 调用 isEmpty 方法并取反，判断数组是否非空
+        return !isEmpty(array);
+    }
+
+    /**
+     * isNotEmpty 判断一个 double 类型的数组是否非空。
+     * 如果数组不为 null 且长度大于 0，则返回 true，表示数组非空；
+     * 否则返回 false，表示数组为空。
+     *
+     * @param array {@link Double[]}要检查的 double 类型数组
+     * @return 如果数组非空则返回 true，否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isNotEmpty(double[] array) {
+        // 调用 isEmpty 方法并取反，判断数组是否非空
+        return !isEmpty(array);
+    }
+
+    /**
+     * isNotEmpty 判断一个 float 类型的数组是否非空。
+     * 如果数组不为 null 且长度大于 0，则返回 true，表示数组非空；
+     * 否则返回 false，表示数组为空。
+     *
+     * @param array {@link Float[]}要检查的 float 类型数组
+     * @return 如果数组非空则返回 true，否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isNotEmpty(float[] array) {
+        // 调用 isEmpty 方法并取反，判断数组是否非空
+        return !isEmpty(array);
+    }
+
+    /**
+     * isNotEmpty 判断一个 boolean 类型的数组是否非空。
+     * 如果数组不为 null 且长度大于 0，则返回 true，表示数组非空；
+     * 否则返回 false，表示数组为空。
+     *
+     * @param array {@link Boolean[]}要检查的 boolean 类型数组
+     * @return 如果数组非空则返回 true，否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isNotEmpty(boolean[] array) {
+        // 调用 isEmpty 方法并取反，判断数组是否非空
+        return !isEmpty(array);
+    }
+
+    /**
      * isEmpty 判断一个整型数组是否为空。
      * 这个方法用于检查传入的整型数组是否为“空”状态。
      * “空”的定义包括两种情况：
@@ -49,6 +148,90 @@ public class PrimitiveArrayUtil {
         // 或者
         // 判断数组的长度是否为 0
         // 如果 array 不为 null，但它的长度是 0，也返回 true 表示“空”
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * isEmpty 检查给定的long类型数组是否为空（null或长度为0）
+     *
+     * @param array {@link Long[]}要检查的long类型数组
+     * @return 如果数组为null或长度为0，则返回true；否则返回false
+     * @author LiuQi
+     */
+    public static boolean isEmpty(long[] array) {
+        // 判断数组是否为null或者长度是否为0
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * isEmpty 检查给定的 double 类型数组是否为空（null 或长度为 0）。
+     *
+     * @param array {@link Double[]} 要检查的 double 类型数组
+     * @return 如果数组为 null 或长度为 0，则返回 true；否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isEmpty(double[] array) {
+        // 判断数组是否为null或者长度是否为0
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * isEmpty 检查给定的 float 类型数组是否为空（null 或长度为 0）。
+     *
+     * @param array {@link Float[]} 要检查的 float 类型数组
+     * @return 如果数组为 null 或长度为 0，则返回 true；否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isEmpty(float[] array) {
+        // 判断数组是否为null或者长度是否为0
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * isEmpty 检查给定的 boolean 类型数组是否为空（null 或长度为 0）。
+     *
+     * @param array {@link Boolean[]} 要检查的 boolean 类型数组
+     * @return 如果数组为 null 或长度为 0，则返回 true；否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isEmpty(boolean[] array) {
+        // 判断数组是否为null或者长度是否为0
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * isEmpty 检查给定的 short 类型数组是否为空（null 或长度为 0）。
+     *
+     * @param array {@link Short[]} 要检查的 short 类型数组
+     * @return 如果数组为 null 或长度为 0，则返回 true；否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isEmpty(short[] array) {
+        // 判断数组是否为null或者长度是否为0
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * isEmpty 检查给定的 char 类型数组是否为空（null 或长度为 0）。
+     *
+     * @param array {@link Character[]} 要检查的 char 类型数组
+     * @return 如果数组为 null 或长度为 0，则返回 true；否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isEmpty(char[] array) {
+        // 判断数组是否为null或者长度是否为0
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * isEmpty 检查给定的 byte 类型数组是否为空（null 或长度为 0）。
+     *
+     * @param array {@link Byte[]} 要检查的 byte 类型数组
+     * @return 如果数组为 null 或长度为 0，则返回 true；否则返回 false
+     * @author LiuQi
+     */
+    public static boolean isEmpty(byte[] array) {
+        // 判断数组是否为null或者长度是否为0
         return array == null || array.length == 0;
     }
 
@@ -116,6 +299,90 @@ public class PrimitiveArrayUtil {
         }
         // 返回结果数组
         return range;
+    }
+
+    /**
+     * max 计算一个可变参数 int 类型数组中的最大值。
+     * 如果传入的数组为空（即 null 或长度为 0），则抛出 IllegalArgumentException 异常。
+     *
+     * @param numberArray {@link Integer[]} 可变参数 int 类型数组
+     * @return 数组中的最大值
+     * @throws IllegalArgumentException 如果数组为空（null 或长度为 0）
+     * @author LiuQi
+     */
+    public static int max(int... numberArray) {
+        // 检查数组是否为空
+        if (isEmpty(numberArray)) {
+            // 如果数组为空，抛出 IllegalArgumentException 异常，提示数组不能为空
+            throw new IllegalArgumentException("Number array must not empty ! ");
+        }
+        // 假设数组的第一个元素为当前最大值
+        int maxNumber = numberArray[0];
+        // 从数组的第二个元素开始遍历
+        for (int i = 1; i < numberArray.length; i++) {
+            // 如果当前元素大于当前最大值，则更新最大值
+            if (maxNumber < numberArray[i]) {
+                maxNumber = numberArray[i];
+            }
+        }
+        // 返回找到的最大值
+        return maxNumber;
+    }
+
+    /**
+     * max 计算一个可变参数 long 类型数组中的最大值。
+     * 如果传入的数组为空（即 null 或长度为 0），则抛出 IllegalArgumentException 异常。
+     *
+     * @param numberArray {@link Long[]} 可变参数 long 类型数组
+     * @return 数组中的最大值
+     * @throws IllegalArgumentException 如果数组为空（null 或长度为 0）
+     * @author LiuQi
+     */
+    public static long max(long... numberArray) {
+        // 检查数组是否为空
+        if (isEmpty(numberArray)) {
+            // 如果数组为空，抛出 IllegalArgumentException 异常，提示数组不能为空
+            throw new IllegalArgumentException("Number array must not empty ! ");
+        }
+        // 假设数组的第一个元素为当前最大值
+        long maxNumber = numberArray[0];
+        // 从数组的第二个元素开始遍历
+        for (int i = 1; i < numberArray.length; i++) {
+            // 如果当前元素大于当前最大值，则更新最大值
+            if (maxNumber < numberArray[i]) {
+                maxNumber = numberArray[i];
+            }
+        }
+        // 返回找到的最大值
+        return maxNumber;
+    }
+
+    /**
+     * max 计算一个可变参数 double 类型数组中的最大值。
+     * 如果传入的数组为空（即 null 或长度为 0），则抛出 IllegalArgumentException 异常。
+     *
+     * @param numberArray {@link Double[]} 可变参数 double 类型数组
+     * @return 数组中的最大值
+     * @throws IllegalArgumentException 如果数组为空（null 或长度为 0）
+     * @author LiuQi
+     */
+    public static double max(double... numberArray) {
+        // 检查数组是否为空
+        if (isEmpty(numberArray)) {
+            // 如果数组为空，抛出 IllegalArgumentException 异常，提示数组不能为空
+            throw new IllegalArgumentException("Number array must not empty ! ");
+        }
+        // 假设数组的第一个元素为当前最大值
+        double maxNumber = numberArray[0];
+        // 从数组的第二个元素开始遍历
+        for (int i = 1; i < numberArray.length; i++) {
+            // 如果当前元素大于当前最大值，则更新最大值
+            if (maxNumber < numberArray[i]) {
+                maxNumber = numberArray[i];
+            }
+        }
+        // 返回找到的最大值
+        return maxNumber;
     }
 
 
